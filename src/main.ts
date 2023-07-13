e7f0e2c3461e2179044ccf38660b82d2ad511504
Daniel-D3V <contatodanielsilvaoficial@gmail.com>
2023-07-13 20:57:40 -0300

    sa

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { CustomValidationPipe } from './filters';
-import "dotenv/config"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log(process.env)
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://admin:admin@localhost:5672'],
+
+    },
+  });
 
   app.useGlobalPipes(new CustomValidationPipe());
   await app.listen(5000);
