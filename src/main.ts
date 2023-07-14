cc6cbd8eb55a10bea2dc5b6276aa0325ca121e23
rethred <gabrielwastha@gmail.com>
2023-07-13 21:29:07 -0300

    change

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { CustomValidationPipe } from './filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  console.log(process.env)
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://admin:admin@localhost:5672'],
-
-    },
-  });
 
   app.useGlobalPipes(new CustomValidationPipe());
   await app.listen(5000);
