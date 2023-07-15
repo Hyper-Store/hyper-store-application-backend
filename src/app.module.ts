f57fc1a7a4a0ec5c21175dc6802e041cccdccb3b
rethred <gabrielwastha@gmail.com>
2023-07-15 20:19:16 -0300

    remove mongo from project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { AuthModule } from './modules/auth/auth.module';
 import { RabbitmqOutboxModule } from './infra/rabbitmq-outbox/rabbitmq-outbox.module';
 import { RabbitModule } from './rabbitmq';
 import { EventSourcingModule } from './infra/event-sourcing/event-sourcing.module';
-import { MongooseService } from './mongoose/mongoose.service';
-import { MongooseModule } from './mongoose/mongoose.module';
 import { SignaturesModule } from './modules/signatures/signatures.module';
 import { ServicesModule } from './modules/services/services.module';
 import { KeysModule } from './modules/keys/keys.module';
@@ -20,7 +18,6 @@ import { UserSessionModule } from './modules/user-session/user-session.module';
     AuthModule,
     RabbitmqOutboxModule,
     EventSourcingModule,
-    MongooseModule,
     SignaturesModule,
     ServicesModule,
     KeysModule,
--- src/mongoose/mongoose.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { MongooseService } from './mongoose.service';
-
-@Global()
-@Module({
-    providers: [MongooseService],
-    exports: [ MongooseService ]
-})
-export class MongooseModule {}
