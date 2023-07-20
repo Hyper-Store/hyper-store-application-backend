7a47bdf3142f17abb2bb0787522ca08a1abc8382
rethred <gabrielwastha@gmail.com>
2023-07-20 13:15:18 -0300

    s

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    allowedHeaders: "*",
     credentials: true
   });
   app.use(cookieParser());
