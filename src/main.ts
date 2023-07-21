5eb1e8551a2d09cc8e7789d2d2b910e047d99d75
rethred <gabrielwastha@gmail.com>
2023-07-20 23:02:06 -0300

    add rate limit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CustomValidationPipe } from './infra/filters';
 import * as cookieParser from 'cookie-parser';
-
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +12,20 @@ async function bootstrap() {
     },
     credentials: true
   });
+
+  app.use(rateLimit({
+    windowMs: 1000, // 1 second
+    max: 5, // limit each IP to 5 requests per windowMs
+    handler: function(req, res) {
+      res.status(429).json({ 
+        error: {
+          message: 'Too many requests, please try again later.',
+          name: "TooManyRequestsError"
+        }
+      });
+    }
+  }));
+
   app.use(cookieParser());
 
   app.useGlobalPipes(new CustomValidationPipe());
