50db9b756c132d6aad3d5a9b8c9b2ee55ee8f8d0
rethred <gabrielwastha@gmail.com>
2023-07-21 15:04:26 -0300

    s

--- src/modules/websocket/websocket.controller.ts
+++ src/modules/websocket/websocket.controller.ts
@@ -66,6 +66,19 @@ export class WebsocketController{
         this.websocketConnectionsService.removeClient(client)
     }
   
+    afterInit(server: Server) {
+        server.on('headers', (headers, req) => {
+          const origin = req.headers.origin;
+    
+          // Check the origin against your list of allowed origins.
+          // Here, we're just checking it against a single allowed origin.
+          const allowedOrigin = 'app://.';
+          if (origin !== allowedOrigin) {
+            // If the origin isn't allowed, we close the connection.
+            req.destroy();
+          }
+        })
+    }
 
 
 }
